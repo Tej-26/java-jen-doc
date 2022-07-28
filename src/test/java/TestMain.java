42747554b2a9260c5297bbc5269468391285e197
Tej-26 <tejasdaidawar260496@gmail.com>
2022-07-28 15:29:59 +0530

    Added java demo application files

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.MavJenDocA.Main;
+
+public class TestMain {
+	@Test
+    public void testInputIsEven(){
+	assertTrue(Main.checkIfInputIsAnEvenNumber(22)); //
+	//Assertion
+	}
+
+	
+	}
+
